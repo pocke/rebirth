bc7e2ec613b8e0a09a83d422073d0aeae848054e
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-07-23 15:54:17 +0900

    DRY spec

--- spec/generators/encoder/install_generator_spec.rb
+++ spec/generators/encoder/install_generator_spec.rb
@@ -2,31 +2,7 @@ require 'spec_helper'
 require 'generators/encoder/install_generator'
 require 'ripper'
 
-describe Encoder::Generators::InstallGenerator do
-  let(:test_case){Class.new(Rails::Generators::TestCase)}
-  subject{test_case.new(:create_encoder_file).run_generator(arguments)}
-  before do
-    test_case.tests described_class
-    test_case.destination_root = SPEC_TMP_DPR
-  end
-
-  after do
-    FileUtils.remove_entry_secure(Pathname.new(SPEC_TMP_DPR)/'app/encoders/', true)
-  end
-
-  shared_examples 'should_valid_as_a_ruby_script' do |filename|
-    it 'should valid as a ruby script' do
-      subject
-      rb = File.read(Pathname.new(SPEC_TMP_DPR)/filename)
-      rip = Class.new(Ripper) do
-        def on_parse_error(msg)
-          raise msg
-        end
-      end
-      rip.parse(rb)
-    end
-  end
-
+describe Encoder::Generators::InstallGenerator, :with_generator do
   context 'simple' do
     let(:arguments){%w[user]}
 
--- spec/support/utils.rb
+++ spec/support/utils.rb
@@ -123,3 +123,29 @@ RSpec.shared_context 'should_serialize' do
     end
   end
 end
+
+RSpec.shared_context 'with_generator', :with_generator do
+  let(:test_case){Class.new(Rails::Generators::TestCase)}
+  subject{test_case.new(:create_encoder_file).run_generator(arguments)}
+  before do
+    test_case.tests described_class
+    test_case.destination_root = SPEC_TMP_DPR
+  end
+
+  after do
+    FileUtils.remove_entry_secure(Pathname.new(SPEC_TMP_DPR)/'app/encoders/', true)
+  end
+end
+
+RSpec.shared_examples 'should_valid_as_a_ruby_script' do |filename|
+  it 'should valid as a ruby script' do
+    subject
+    rb = File.read(Pathname.new(SPEC_TMP_DPR)/filename)
+    rip = Class.new(Ripper) do
+      def on_parse_error(msg)
+        raise msg
+      end
+    end
+    rip.parse(rb)
+  end
+end
