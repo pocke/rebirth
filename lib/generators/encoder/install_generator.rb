dacbd60d0fad3b95c2ded2c71264a4f507359bdf
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-07-23 15:26:19 +0900

    bin/rails g encoder:install

--- lib/generators/encoder/install_generator.rb
+++ lib/generators/encoder/install_generator.rb
@@ -2,23 +2,19 @@ require 'rails/generators'
 
 module Encoder
   module Generators
-    class EncoderGenerator < Rails::Generators::NamedBase
+    class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
       check_class_collision suffix: 'Encoder'
 
       def create_encoder_file
-        template 'encoder.rb.erb', File.join('app/encoders', "#{file_name}_encoder.rb")
+        template 'encoder.rb.erb', File.join('app/encoders', "application_encoder.rb")
       end
 
 
       private
 
       def parent_class_name
-        if defined?(::ApplicationEncoder)
-          'ApplicationEncoder'
-        else
-          'Rebirth::MethodBase'
-        end
+        'Rebirth::MethodBase'
       end
     end
   end
