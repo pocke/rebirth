70ad78a5ee4873bf496d2bbb583be2156c6a6ee7
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-07-23 14:08:12 +0900

    Fix rubocop issue

--- lib/generators/encoder/encoder_generator.rb
+++ lib/generators/encoder/encoder_generator.rb
@@ -4,7 +4,7 @@ module Encoder
   module Generators
     class EncoderGenerator < Rails::Generators::NamedBase
       source_root File.expand_path('../templates', __FILE__)
-      check_class_collision :suffix => 'Encoder'
+      check_class_collision suffix: 'Encoder'
 
       def create_encoder_file
         template 'encoder.rb.erb', File.join('app/encoders', class_path, "#{file_name}_encoder.rb")
